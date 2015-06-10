b875e6d9a1c4aa820086b6c69392259a74ad0fe7
Raymond <Rayrohder@gmail.com>
2015-06-10 11:07:06 +0200

    added some classes

--- FormalMethods/Source/Model/DFA.cs
+++ FormalMethods/Source/Model/DFA.cs
@@ -1,1 +1,45 @@
+using System.Collections;
+namespace Models
+{
+    class DFA
+    {
+        string input;
+        ArrayList nodes;
 
+        public void setInput(string newInput)
+        {
+            input = newInput;
+        }
+
+        public string getInput()
+        {
+            return input;
+        }
+
+        public void setNodes(ArrayList newNodes)
+        {
+            nodes = newNodes;
+        }
+
+        public void addNode(Node node)
+        {
+            nodes.Add(node);
+        }
+
+        public ArrayList getNodes()
+        {
+            return nodes;
+        }
+
+        public DFA minimalize(DFA dfa)
+        {
+            return dfa;
+        }
+
+        public NDFA reverse(DFA dfa)
+        {
+            NDFA ndfa = new NDFA();
+            return ndfa;
+        }
+    }
+}
